e18431b0b458b99f813db5ffffd54ef909505350
Christopher Chang <cthomaschang@gmail.com>
2024-03-23 21:23:04 -0400

    feature: Add table of elements data, tech spec and implement element search
    [chemistryToolkit.py]:
    -Implement element search by name, symbol or number
    -TODO: make search not dependent on case

---
+++ chemistryToolkit/chemistryToolkit.py
@@ -0,0 +1,27 @@
+# Chemistry Toolkit
+# Main function
+# Takes user input and return elemental properties
+
+import csv
+
+# Open the library of elements and save it as a list without the first
+# row
+with open('Periodic Table of Elements.csv', newline = '') as csvfile:
+    reader = csv.reader(csvfile)
+    tableOfElements = list(reader)
+    tableOfElements = tableOfElements[1:]
+        
+# Get desired element from user
+userElement = input("Enter an element number, name or symbol: ")
+
+def elementIndex(data, query):
+    for subList in data:
+        if query == subList[0] or query == subList[1] or query == subList[2]:
+            return data.index(subList)
+    raise ValueError("'{query}' is not in the table")
+
+tableRow = elementIndex(tableOfElements, userElement)
+
+print("Element:",tableOfElements[tableRow][1])
+print("Atomic Number:",tableOfElements[tableRow][0])
+print("Atomic Mass:",tableOfElements[tableRow][2])
