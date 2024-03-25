2afe78ee83aacf3eceddae429cfdcca2c03d45dd
Christopher Chang <cthomaschang@gmail.com>
2024-03-24 21:27:53 -0400

    refactor: Split program into multiple files

--- chemistryToolkit/chemistryToolkit.py
+++ chemistryToolkit/chemistryToolkit.py
@@ -3,11 +3,7 @@
 # Takes user input and return elemental properties
 
 import csv
-
-cElementNumberIndex = 0
-cElementNameIndex   = 1
-cElementSymbolIndex = 2
-cElementMassIndex   = 3
+import elementSearch
 
 # Open the library of elements and save it as a list without the first
 # row
@@ -16,21 +12,4 @@ with open('Periodic Table of Elements.csv', newline = '') as csvfile:
     tableOfElements = list(reader)
     tableOfElements = tableOfElements[1:]
         
-# Get desired element from user
-userElement = input("Enter an element number, name or symbol: ")
-
-def elementIndex(data, query):
-    for subList in data:
-        if (query == subList[cElementNumberIndex]
-        or query.lower() == subList[cElementNameIndex].lower()
-        or query.lower() == subList[cElementSymbolIndex].lower()):
-            return data.index(subList)
-    raise ValueError("'{query}' is not in the table")
-
-tableRow = elementIndex(tableOfElements, userElement)
-
-print("Element:      ",tableOfElements[tableRow][cElementNameIndex])
-print("Atomic Number:",tableOfElements[tableRow][cElementNumberIndex])
-print("Atomic Mass:  ",tableOfElements[tableRow][cElementMassIndex])
-
-input("Press Enter to continue...")
+elementSearch.searchForElement(tableOfElements)
---
+++ chemistryToolkit/columnIndices.py
@@ -0,0 +1,7 @@
+# Column Indices
+# Define the row index of each elemental property
+
+ElementNumber = 0
+ElementName   = 1
+ElementSymbol = 2
+ElementMass   = 3
